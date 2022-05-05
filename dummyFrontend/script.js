6291b80276a2e75b4902e0b2d932ee226a91d24d
James-Dugo <james.b.dugo@gmail.com>
2022-05-05 23:33:30 +1000

    Buttons added

--- dummyFrontend/script.js
+++ dummyFrontend/script.js
@@ -1,13 +1,11 @@
 // Exploring URL Hashes
 import { split_hash } from "./util.js"
-import { homeView, movieListView, movieView } from "./views.js"
+import { homeView, addNewMovieView, movieListView, movieView } from "./views.js"
 
 // allMovies is a global variable that will hold the array
 // of Movies loaded from Strapi
 let allMovies = []
-
 const dbUrl = 'http://localhost:1337/api/movies?populate=*'
-
 
 // loadData will load the data using fetch
 // and call redraw() when it is ready
@@ -18,7 +16,6 @@ const loadData = () => {
     })
     .then((data) => {
         allMovies = data.data
-        console.log("allMovies: ",allMovies)
         redraw()
     })
 }
@@ -37,25 +34,27 @@ function sortMovies() {
     return sortedMovies
 }
 
-function getMoviesInSession (session) {
-    let moviesInSession = []
+function getMoviesWatched (flag) {
+    let moviesWatched = []
     for(let i=0; i<allMovies.length;i++) {
-        if (allMovies[i].attributes.offering === session){
-            moviesInSession.push(allMovies[i])
+        if (allMovies[i].attributes.watched&flag){
+            moviesWatched.push(allMovies[i])
+        } else{
+            moviesWatched.push(allMovies[i])
         }
     }
-    return moviesInSession
+    return movieWatched
 }
 
-function buttonHandler(buttonid, movieType){
+function buttonHandler(buttonid, buttonType){
     let button=document.getElementById(buttonid)
     let data = []
     button.onclick = function () {
-        if (movieType === 'S1' || movieType === 'S2'){
-            data=getMoviesInSession(movieType)
-        } else if (movieType === 'All'){
+        if (buttonType === 'Watched' || buttonType === 'S2'){
+            data = getMoviesInSession(buttonType)
+        } else if (buttonType === 'All'){
             data = getMovies()
-        } else if (movieType === 'Sorted'){
+        } else if (buttonType === 'Sorted'){
             data = sortMovies()
         }
         movieListView('content', data)
@@ -70,13 +69,59 @@ function bindings(){
     buttonHandler('action4','Sorted')
 }
 
+const btn = document.querySelector('button');
+
+function sendData( data ) {
+  console.log( 'Sending data' );
+
+  const XHR = new XMLHttpRequest();
+
+  let urlEncodedData = "",
+      urlEncodedDataPairs = [],
+      name;
+
+  // Turn the data object into an array of URL-encoded key/value pairs.
+  for( name in data ) {
+    urlEncodedDataPairs.push( encodeURIComponent( name ) + '=' + encodeURIComponent( data[name] ) );
+  }
+
+  // Combine the pairs into a single string and replace all %-encoded spaces to
+  // the '+' character; matches the behavior of browser form submissions.
+  urlEncodedData = urlEncodedDataPairs.join( '&' ).replace( /%20/g, '+' );
+
+  // Define what happens on successful data submission
+  XHR.addEventListener( 'load', function(event) {
+    alert( 'Yeah! Data sent and response loaded.' );
+  } );
+
+  // Define what happens in case of error
+  XHR.addEventListener( 'error', function(event) {
+    alert( 'Oops! Something went wrong.' );
+  } );
+
+  // Set up our request
+  XHR.open( 'POST', 'https://example.com/cors.php' );
+
+  // Add the required HTTP header for form data POST requests
+  XHR.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+
+  // Finally, send our data.
+  XHR.send( urlEncodedData );
+}
+
+btn.addEventListener( 'click', function() {
+  sendData( allMovies[0] );
+} )
+
 // redraw is called whenever the page needs to be 
 // updated, it calls the appropriate view function
 const redraw = () => {
 
     const pathInfo = split_hash(window.location.hash)
 
-    if (pathInfo.path === "movies") {
+    if (pathInfo.path === "movieform"){
+        addNewMovieView('content')
+    }else if (pathInfo.path === "movies") {
         if (pathInfo.id){
             const movie = getMovie(pathInfo.id)
             movieView('content',movie)
--- dummyFrontend/views.js
+++ dummyFrontend/views.js
@@ -1,26 +1,54 @@
+const url="http://localhost:1337"
+
+export const addNewMovieView = (id) => {
+    const template = Handlebars.compile(`
+    <form action="/test" method="post">
+        <ul>
+            <li>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="user_name">
+            </li>
+            <li>
+                <label for="mail">E-mail:</label>
+                <input type="email" id="mail" name="user_email">
+            </li>
+            <li>
+                <label for="msg">Message:</label>
+                <textarea id="msg" name="user_message"></textarea>
+            </li>
+            <li class="button">
+                <button type="submit">Send your message</button>
+            </li>
+        </ul>
+    </form>
+    <button>Click Me!</button>
+    `)
+    const target = document.getElementById(id)
+    target.innerHTML = template()
+}
 
 export const movieListView = (id, movieArray) => {
 
     const template = Handlebars.compile(`
-      <table>
-      <tr>
-      <th>Title</th>
-      <th>Watched</th>
-      <th>Rating</th>
-      <th>Poster</th>
-          {{#each movies}}
-              <tr>
-                  <td> <a href="#!/movies/{{id}}"> {{attributes.title}}</a> </td>
-                  <td> {{attributes.watched}} </td>
-                  <td> {{attributes.rating}} </td>
-                  <td> <img src="http://localhost:1337{{attributes.poster.data.attributes.formats.thumbnail.url}}"></td>
-              </tr>
-          {{/each}}
-      </table>
-      <button id="action1">S1 Units</button>
-      <button id="action2">S2 Units</button>
-      <button id="action3">All Units</button>
-      <button id="action4">Sorted Units</button>
+    <button id="action1">S1 Units</button>
+    <button id="action2">S2 Units</button>
+    <button id="action3">All Units</button>
+    <button id="action4">Sorted Units</button>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Watched</th>
+            <th>Rating</th>
+            <th>Poster</th>
+        {{#each movies}}
+        <tr>
+            <td> <a href="#!/movies/{{id}}"> {{attributes.title}}</a> </td>
+            <td> {{attributes.watched}} </td>
+            <td> {{attributes.rating}} </td>
+            <td> <img src="http://localhost:1337{{attributes.poster.data.attributes.formats.thumbnail.url}}" width="110" height="155.99"></td>
+        </tr>
+        {{/each}}
+    </table>
     `)
 
     const target = document.getElementById(id)
@@ -36,7 +64,7 @@ export const homeView = (id,movieArray) => {
     <div class="movie-displays">
     <ul>
         {{#each movies}}
-            <a href="#!/movies/{{id}}">{{attributes.code}}</a>
+            <li><a href="#!/movies/{{id}}"><img src="http://localhost:1337{{attributes.poster.data.attributes.formats.small.url}}"></a></li>
         {{/each}}
     </ul>
     </div>
@@ -49,7 +77,7 @@ export const homeView = (id,movieArray) => {
 export const movieView = (id,movie) => {
     const template = Handlebars.compile (`
     <h1>{{movie.attributes.title}}</h1>
-    <img src="http://localhost:1337{{movie.attributes.poster.data.attributes.formats.medium.url}}">
+    <img src="{{url}}{{movie.attributes.poster.data.attributes.formats.medium.url}}">
     <table>
     <tr>
     <th>Genres</th>
@@ -65,5 +93,5 @@ export const movieView = (id,movie) => {
     </table>
     `)
     const target = document.getElementById(id)
-    target.innerHTML=template({movie:movie})
+    target.innerHTML=template({movie:movie,url:url})
 }
