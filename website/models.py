1f5158620c385c759d58484188f9de96c29212f5
James D'Ugo <james.dugo@students.mq.edu.au>
2023-03-29 23:47:43 +1100

    slight restructure to fix relationships

--- website/models.py
+++ website/models.py
@@ -6,21 +6,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(256))
-    data = db.relationship("Rating")
+    data = db.relationship("Data")
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300))
     year = db.Column(db.Integer)
     genre = db.Column(db.String(60))
-    ratings = db.relationship("Rating")
+    ratings = db.relationship("Data")
     average_rating = db.Column(db.Float)
     poster = db.Column(db.String(100))
     synopsis = db.Column(db.String(10000))
 
-class Rating(db.Model):
+class Data(db.Model):
     movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"),primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"),primary_key=True)
     rating = db.Column(db.Integer)
+    watched = db.Column(db.Boolean)
 
 
