b444659b33aea41fdbd82fa11b0b4474c39b1620
James D'Ugo <james.dugo@students.mq.edu.au>
2023-03-30 14:33:27 +1100

    Scope creep avoided

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # watchlist-webapp
-This webapp is made as a showcase for my partner and I. James & Rosaria's cinema. An art deco cinema experience for all the people in todays world wanting to keep track of the movies that they've watched.
+This webapp is made as a showcase for my partner and I. James & Rosaria's cinema. An art deco list of all of the movies that we have watched and are planning on watching
--- website/models.py
+++ website/models.py
@@ -6,7 +6,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(256))
-    data = db.relationship("Data")
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,14 +13,9 @@ class Movie(db.Model):
     year = db.Column(db.Integer)
     genre = db.Column(db.String(60))
     ratings = db.relationship("Data")
-    average_rating = db.Column(db.Float)
     poster = db.Column(db.String(100))
     synopsis = db.Column(db.String(10000))
+    watched = db.Column(db.Boolean())
 
-class Data(db.Model):
-    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"),primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"),primary_key=True)
-    rating = db.Column(db.Integer)
-    watched = db.Column(db.Boolean)
 
 
